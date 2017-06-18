3790f3c640d36472afd288ae1740ce757a08a740
Scott Wade <sc0ttwad3@gmail.com>
2017-06-18 14:16:17 -0400

    Customizations to the template and default to system fonts

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Create-React-App Rewired</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="header-text">
+          <h3>Header Intro</h3>
+          <p className="App-intro">
+            This is header-text simply to demonstrate the app. To get started customizing this template, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
